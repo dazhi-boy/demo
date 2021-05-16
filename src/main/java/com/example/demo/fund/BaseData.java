1b98b3c089f323b79efb334cd48c90448825ac46
dazhi-boy <986095195@qq.com>
2021-05-16 23:46:57 +0800

    基金

--- src/main/java/com/example/demo/fund/BaseData.java
+++ src/main/java/com/example/demo/fund/BaseData.java
@@ -1,12 +1,9 @@
 package com.example.demo.fund;
 
-import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -18,20 +15,20 @@ public class BaseData {
     private static Double principalSum = 100D;//总本金
     private static Double actualSum = 100D;//最终金额
     private static Double principal = 100D;//定投金额
+    private static Double targetAssertionUp = 100D;//目标收益率
+    private static Double targetAssertionDoen = -2D;//目标收益率
     private static Date start;//开始时间 2009-09-30
     private static Date end;//结束时间，结束时间要小于2021-05-14
+    private static int interval;//间隔几年
     private static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     private static Calendar rightNow = Calendar.getInstance();
     static {
         try {
-            principalSum = 100D;
-            actualSum = 100D;
-            principal = 100D;
-            start = dateFormat.parse("2015-05-14");
-            rightNow.setTime(start);
-            rightNow.add(Calendar.YEAR, 1);
-            end = rightNow.getTime();
-//            end = dateFormat.parse("2017-05-14");
+            targetAssertionDoen = -1D;//止损
+            targetAssertionUp = 200D;//止盈
+            interval = 1;
+            start = dateFormat.parse("2008-01-01");
+//            start = dateFormat.parse("2008-05-14");
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -39,26 +36,52 @@ public class BaseData {
 
     public static void main(String[] args) throws IOException {
         Entity entity = DataUtil.get("src/main/java/com/example/demo/fund/data/160119.txt");//南方中证500ETF联接A
+        for (int i=0;i>-15;i--) {
+            principalSum = 100D;
+            actualSum = 100D;
+            principal = 100D;
+            rightNow.setTime(start);
+            rightNow.add(Calendar.YEAR, interval);
+            start = rightNow.getTime();
+            getProfit(entity, start);
+            System.out.println();
+        }
+    }
+
+    private static void getProfit(Entity entity, Date start) {
+        rightNow.setTime(start);
+        rightNow.add(Calendar.YEAR, interval);
+        end = rightNow.getTime();
 
         NumberFormat nf = NumberFormat.getNumberInstance();
         nf.setGroupingUsed(false);
-        nf.setMaximumFractionDigits(2);
+        nf.setMaximumFractionDigits(4);
         for (Entity.Item item : entity.LSJZList) {
             Date FSRQ = item.FSRQ;//日期
             if (FSRQ.after(start) && FSRQ.before(end)) {
                 String JZZZL = item.JZZZL;//收益率
                 if (StringUtils.isEmpty(JZZZL)) {
-                    System.out.println("发现一个没收益率的" + FSRQ);
+//                    System.out.println("发现一个没收益率的" + FSRQ);
                     continue;
                 }
                 Double rate = new Double(JZZZL);
                 actualSum += actualSum * rate / 100;
                 actualSum = new Double(nf.format(actualSum));
-                actualSum += principal;//最终金额
-                principalSum += principal;//本金
+
+//                actualSum += principal;//最终金额
+//                principalSum += principal;//本金
+
+                if (rate < targetAssertionDoen) {
+                    actualSum += principal;//最终金额
+                    principalSum += principal;//本金
+                }
+                /*if (rate >  targetAssertionUp){
+                    actualSum -= principal;//最终金额
+                    principalSum -= principal;//本金
+                }*/
             }
         }
-        System.out.println("本金："+principalSum+"    最终金额："+actualSum);
-        System.out.println("收益："+ (actualSum - principalSum) +"    收益率："+ (actualSum - principalSum)/actualSum);
+//        System.out.print("本金：%f"+principalSum+"    最终金额："+actualSum);
+        System.out.format("日期：%s    \t本金：%.4f\t    最终金额：%.4f\t    收益：%.4f\t    收益率：%.4f",(dateFormat.format(start)+"~"+dateFormat.format(end)), principalSum,actualSum,(actualSum - principalSum),((actualSum - principalSum)/actualSum));
     }
 }
